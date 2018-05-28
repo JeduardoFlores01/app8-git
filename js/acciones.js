01ffddc620c2f1ef8c41d3b5405774be18bc9a07
Juan Eduardo Flores <jude_fm@hotmail.com>
2018-05-28 13:12:06 -0500

    6subida

--- js/acciones.js
+++ js/acciones.js
@@ -29,3 +29,36 @@ $(document).ready(function (e){
 function escribehistoria(accion){
 	$('#eHistoria').append('<li>'+accion+'</li');
 }
+
+$(document).ready(function(e){
+	document.addEventListener("deviceready",function(){
+		$('#beep').tap(function(){
+			navigator.notification.beep(2);
+		}); // Cierre del tap de beep
+		$('#vibrar').tap(function(){
+			navigator.notification.vibrate(2000);
+		}); // Cierre del tap de vibrar
+  }, false); // Cierre de EventListener
+}); // Cierre de la funcion principal
+
+$(document).ready(function(e) {
+    document.addEventListener("deviceready", function(){
+		$('#izquierda').on("swipeleft",function(){
+			navigator.notification.alert("Deslizó a la izquierda",function(){"aplicacion7","Aceptar"});	
+         });//cierre deslizar izquierda
+		 $('#derecha').on("swiperight",function(){
+			 navigator.notification.confirm("¿Que quieres hacer?",function(opt){
+				 switch(opt)
+				 {
+					 case 1:
+					 navigator.notification.beep(2);
+					 break;
+					 
+					 case 2:
+					 navigator.notification.vibrate(2000);
+					 break;
+				 }
+			 },"aplicacion7","Beepear,Vibrar,Cancelar");
+		 });//cierre del deslizar derecha
+	},false);//cierre del deviceready
+});//Cierre del document principal
